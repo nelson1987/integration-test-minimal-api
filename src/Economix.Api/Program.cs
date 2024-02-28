0b7fd9c2850b655a0bb7b8b439f08be48c7cdc18
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-02-28 15:07:24 -0300

    Hello World Integration Tests

--- src/Economix.Api/Program.cs
+++ src/Economix.Api/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!_GET");
+app.MapPost("/", () => "Hello World!_POST");
+app.MapPut("/", () => "Hello World!_PUT");
+app.MapDelete("/", () => "Hello World!_DELETE");
 
 app.Run();
--- test/Economix.Tests/HelloWorldIntegrationTests.cs
+++ test/Economix.Tests/HelloWorldIntegrationTests.cs
@@ -1,18 +1,58 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text;
 
 namespace Economix.Tests;
 
 public class HelloWorldIntegrationTests
 {
-    [Fact]
-    public async Task HelloWorld_Result_Is_HelloWorld()
+    public readonly HttpClient httpClient;
+    public HelloWorldIntegrationTests()
     {
         var webAppFactory = new WebApplicationFactory<Program>();
-        var httpClient = webAppFactory.CreateDefaultClient();
+        httpClient = webAppFactory.CreateDefaultClient();
+    }
 
+    [Fact]
+    public async Task HelloWorld_GET_Result_Is_HelloWorld()
+    {
         var response = await httpClient.GetAsync("/");
+        response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadAsStringAsync();
 
-        Assert.Equal("Hello World!", result);
+        Assert.Equal("Hello World!_GET", result);
+    }
+
+    [Fact]
+    public async Task HelloWorld_POST_Result_Is_HelloWorld()
+    {
+        var request = new StringContent("", Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync("/", request);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("Hello World!_POST", result);
+    }
+
+    [Fact]
+    public async Task HelloWorld_PUT_Result_Is_HelloWorld()
+    {
+        var request = new StringContent("", Encoding.UTF8, "application/json");
+        var response = await httpClient.PutAsync("/", request);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("Hello World!_PUT", result);
+    }
+
+    [Fact]
+    public async Task HelloWorld_DELETE_Result_Is_HelloWorld()
+    {
+        var response = await httpClient.DeleteAsync("/");
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("Hello World!_DELETE", result);
     }
 }
