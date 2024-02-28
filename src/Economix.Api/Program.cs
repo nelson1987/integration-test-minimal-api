e5b7fcebd90495cfb3111bdee9c669ed19b57f4e
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-02-28 15:28:26 -0300

    FUNCIONAL :: CRUD de Integration tests

--- src/Economix.Api/Program.cs
+++ src/Economix.Api/Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!_GET");
-app.MapPost("/", () => "Hello World!_POST");
-app.MapPut("/", () => "Hello World!_PUT");
-app.MapDelete("/", () => "Hello World!_DELETE");
+app.MapGet("/", () => Results.Ok("Hello World!_GET"));
+app.MapPost("/", () => Results.Created("/", "Hello World!_POST"));
+app.MapPut("/", () => Results.Accepted("/", "Hello World!_PUT"));
+app.MapDelete("/", () => Results.NoContent());
 
 app.Run();
--- test/Economix.Tests/HelloWorldIntegrationTests.cs
+++ test/Economix.Tests/HelloWorldIntegrationTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
 using System.Text;
 
 namespace Economix.Tests;
@@ -13,17 +14,18 @@ public class HelloWorldIntegrationTests
     }
 
     [Fact]
-    public async Task HelloWorld_GET_Result_Is_HelloWorld()
+    public async Task HelloWorld_GET_Result_Is_Ok_With_HelloWorld()
     {
         var response = await httpClient.GetAsync("/");
         response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadAsStringAsync();
 
-        Assert.Equal("Hello World!_GET", result);
+        Assert.Equal((int)HttpStatusCode.OK, (int)response.StatusCode);
+        Assert.Equal("\"Hello World!_GET\"", result);
     }
 
     [Fact]
-    public async Task HelloWorld_POST_Result_Is_HelloWorld()
+    public async Task HelloWorld_POST_Result_Is_Created_With_HelloWorld()
     {
         var request = new StringContent("", Encoding.UTF8, "application/json");
         var response = await httpClient.PostAsync("/", request);
@@ -31,11 +33,12 @@ public class HelloWorldIntegrationTests
 
         var result = await response.Content.ReadAsStringAsync();
 
-        Assert.Equal("Hello World!_POST", result);
+        Assert.Equal((int)HttpStatusCode.Created, (int)response.StatusCode);
+        Assert.Equal("\"Hello World!_POST\"", result);
     }
 
     [Fact]
-    public async Task HelloWorld_PUT_Result_Is_HelloWorld()
+    public async Task HelloWorld_PUT_Result_Is_Accepted_With_HelloWorld()
     {
         var request = new StringContent("", Encoding.UTF8, "application/json");
         var response = await httpClient.PutAsync("/", request);
@@ -43,16 +46,16 @@ public class HelloWorldIntegrationTests
 
         var result = await response.Content.ReadAsStringAsync();
 
-        Assert.Equal("Hello World!_PUT", result);
+        Assert.Equal((int)HttpStatusCode.Accepted, (int)response.StatusCode);
+        Assert.Equal("\"Hello World!_PUT\"", result);
     }
 
     [Fact]
-    public async Task HelloWorld_DELETE_Result_Is_HelloWorld()
+    public async Task HelloWorld_DELETE_Result_Is_NoContent()
     {
         var response = await httpClient.DeleteAsync("/");
         response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadAsStringAsync();
 
-        Assert.Equal("Hello World!_DELETE", result);
+        Assert.Equal((int)HttpStatusCode.NoContent, (int)response.StatusCode);
     }
 }
